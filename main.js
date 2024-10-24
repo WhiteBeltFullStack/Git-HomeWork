a5065695daf2acf914bba347f2faecd59af0ff34
WhiteBeltFullStack <zangetsu923@gmail.com>
2024-10-24 17:13:48 +0300

    Add advanced functionality to the onBallClick function

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 'use strict'
 
-function onBallClick(elBall){
-    console.log('this is a ball');
-    
+function onBallClick(elBall) {
+  var currHeight = parseInt(elBall.style.height) || 100
+  var currWidth = parseInt(elBall.style.width) || 100
 
+
+  elBall.style.height = currHeight + 50 + 'px'
+  elBall.style.width = currWidth + 50 + 'px'
+
+  elBall.innerText =  parseInt(elBall.style.height)
+  
 }
+
+// 10.Change onBallClick() to increment the ball’s
+//  width & height by 50px and
+// display the updated diameter
+// inside the ball. Remove the console.log(). ⇩
