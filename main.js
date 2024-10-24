a37093ca9a749575aa45b831a28df8aa2b2ae21e
WhiteBeltFullStack <zangetsu923@gmail.com>
2024-10-24 20:16:49 +0300

    limit the div diameter to 400px max

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ function onBallClick(elBall) {
   var currHeight = parseInt(elBall.style.height) || 100
   var currWidth = parseInt(elBall.style.width) || 100
 
+  if (currHeight >= 400) {
+    currHeight = 50
+    currWidth = 50
+  }
 
   elBall.style.height = currHeight + 50 + 'px'
   elBall.style.width = currWidth + 50 + 'px'
 
-  elBall.innerText =  parseInt(elBall.style.height)
-  
+  elBall.innerText = parseInt(elBall.style.height)
 }
