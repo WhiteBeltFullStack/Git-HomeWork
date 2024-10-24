520fe2b13a443fb8a7a75eca6a9317bb82c433fb
WhiteBeltFullStack <zangetsu923@gmail.com>
2024-10-25 00:20:44 +0300

    Finish last section in part one

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
 'use strict'
 
-function onBallClick(elBall) {
+function onBallClick(elBall, maxDiameter) {
   var currHeight = parseInt(elBall.style.height) || 100
   var currWidth = parseInt(elBall.style.width) || 100
 
-  if (currHeight >= 400) {
-    currHeight = 50
-    currWidth = 50
+  if (currHeight < maxDiameter) {
+    elBall.style.height = currHeight + getRandomInt(20, 61) + 'px'
+    elBall.style.width = currWidth + getRandomInt(20, 61) + 'px'
+}else{
+    elBall.style.height = 100 + 'px'
+    elBall.style.width = 100 + 'px'
   }
 
-  elBall.style.height = currHeight + getRandomInt(20, 61) + 'px'
-  elBall.style.width = currWidth + getRandomInt(20, 61) + 'px'
   elBall.style.backgroundColor = getRandomColor()
   elBall.innerText = parseInt(elBall.style.height)
 }
